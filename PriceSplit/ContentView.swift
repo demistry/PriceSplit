423d7c2033a16e6e44cd37ed21cae1a808576127
David Ilenwabor <dave11ilen@gmail.com>
2019-10-09 23:43:52 +0100

    implemented price splitting

--- PriceSplit/ContentView.swift
+++ PriceSplit/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    private var splitAmount : Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let amountDouble = Double(checkAmount) ?? 0.0
+        let totalAmount = (tipSelection * amountDouble/100) + amountDouble
+        return totalAmount / peopleCount
+    }
+    
+    let tipPercentages = [10,15,20,25,0]
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount).keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header : Text("How much tip do you want to leave?")){
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(0 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section{
+                    Text("$\(splitAmount, specifier: "%.2f")")
+                }
+            }.navigationBarTitle("Price Split")
+        }
     }
 }
 
